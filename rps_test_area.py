e9eadf95ed5569d1149a269b38b77eb1a50c2936
pramika Chockalingam Balaji <pramikabalaji@gmail.com>
2022-02-13 22:41:54 +0000

    new code with new changes

--- rps_test_area.py
+++ rps_test_area.py
@@ -305,7 +305,6 @@ cv2.putText (frame, str(draw), (115, 350), Cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 25
 
 
 #%%
-
 import cv2
 import numpy as np
 import random 
@@ -314,7 +313,7 @@ import time
 t = time.time()
 cap = cv2.VideoCapture(0)
 
-count = 3
+count = 5
 while True:
     
     ret, frame = cap.read()
@@ -339,6 +338,7 @@ import sys
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
 player_points = int()
 computer_points =int()
 game =int()
@@ -356,59 +356,23 @@ def countdown(t):
 '''
 
 
-game_options= [ "rock", "paper", "scissors"]
+#print (f" computer's choice is {computer_input}")
 
-def countdown():
+
+#count = 3
     
-    computer_input = random.choice(game_options)
-    #print (f" computer's choice is {computer_input}")
-    t=20
-    while t: #in range(5):
+t = time.time()
+cap = cv2.VideoCapture(0)
 
 
-    
-        cv2.putText(frame, computer_input, (392, 363), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),3,cv2.LINE_4)
+def play():
 
-        rock1 = prediction[0][0] > 0.7
-        paper1 = prediction[0][1] > 0.7
-        scissors1 = prediction[0][2] > 0.7
-        nothing1 = prediction[0][3] > 0.7 
-
-        prediction1=[rock1, paper1, scissors1, nothing1]
-
-
-        if prediction1[0] == True:
-            player_choice = "rock1"
-        elif prediction1[1] == True:
-            player_choice = "paper1"
-        elif prediction1[2] == True:
-            player_choice = "scissors1"
-        else:
-            player_choice = "Try Again"    
-
-
-        #print (f" player's choice is {player_input}")
-
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}'.format(secs)
-        print(timer, end="\r")
-        time.sleep(0.01)
-        t -= 1
-    
-    player_input = player_choice
-
-    #cv2.putText(frame,(timer), (250,250),cv2.FONT_HERSHEY_SIMPLEX, 1.7, (0, 0, 0),5, cv2.LINE_4)
-
-    #cv2.putText(frame,'player:', (25, 410), cv2. FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
-    cv2.putText (frame,player_input, (75, 363), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),3)
-
-    
-
-    global player_points 
+    global player_input
+    global computer_input
+    global player_points
     global computer_points
-    global game 
-    
-    
+    global game
+
     if (player_input =="paper1" and computer_input == "rock")  or (player_input =="rock1" and computer_input == "scissors")  or (player_input == "scissors1" and computer_input == "paper") :
         print("player wins")    
         player_points +=1
@@ -422,30 +386,20 @@ def countdown():
         print("computer wins")
         computer_points +=1
         game +=1
-        
+            
 
     else :
         print('try again')
 
 
-
     cv2.putText(frame, str(player_points), (117, 471), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),3)
 
     cv2.putText (frame, str(computer_points), (445, 471), cv2. FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),3)
-    
+        
 
     cv2.putText (frame, str(game), (332, 32), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0),3)
 
-    #cv2.waitKey(1) == ord('e')
-
-    #print(f"computer points is {type(computer_points)}")
-    #print(f"player point is {type(player_points)}")
-    #print(f"game: {type(game)}")
-
-
-
-
-while player_points or computer_points <=3: 
+while True: 
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
     image_np = np.array(resized_frame)
@@ -459,14 +413,79 @@ while player_points or computer_points <=3:
     #cv2.putText(frame, 'player points:',(25, 380), cv2. FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
     #cv2.putText(frame, 'computer points: ', (25, 325), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
     cv2.putText (frame,'number of games: ', (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),5)
+    #print (f" player's choice is {player_input}")
+    #ret, frame = cap.read()
+    
+    game_options= [ "rock", "paper", "scissors"]
+   
+    computer_input = random.choice(game_options)
+        
+    cv2.putText(frame, computer_input, (392, 363), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),3,cv2.LINE_4)
+
+    rock1 = prediction[0][0] > 0.7
+    paper1 = prediction[0][1] > 0.7
+    scissors1 = prediction[0][2] > 0.7
+    nothing1 = prediction[0][3] > 0.7 
+
+    prediction1=[rock1, paper1, scissors1, nothing1]
+
+
+    if prediction1[0] == True:
+        player_choice = "rock1"
+    elif prediction1[1] == True:
+        player_choice = "paper1"
+    elif prediction1[2] == True:
+        player_choice = "scissors1"
+    else:
+        player_choice = "Try Again"    
 
 
 
+    player_input = player_choice
 
-    countdown()
+    #cv2.putText(frame,(timer), (250,250),cv2.FONT_HERSHEY_SIMPLEX, 1.7, (0, 0, 0),5, cv2.LINE_4)
 
+    #cv2.putText(frame,'player:', (25, 410), cv2. FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
+    cv2.putText (frame,player_input, (75, 363), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),3)
+      
+    t_elapsed = time.time() - t
+    timer = int(count - round(t_elapsed, 0))
+    cv2.putText(frame, str(timer), (52,457),cv2.FONT_HERSHEY_SIMPLEX, 1.7, (0, 0, 0), 5, cv2.LINE_4)
+    if timer == 0:
+         #t = time.time()
+         play()
+
+    #cv2.imshow('frame', frame)
+        
+    
 
     cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+    '''
+    if player_points==3 and computer_points==3:
+        print("Draw")
+    elif (player_points<3) or (computer_points<3):
+        play()
+    elif player_points>3:
+        print("player wins")
+    else:
+        print("computer wins")
+    '''
+
+
+    #cv2.waitKey(1) == ord('e')
+
+    #print(f"computer points is {type(computer_points)}")
+    #print(f"player point is {type(player_points)}")
+    #print(f"game: {type(game)}")
+
+
+    #countdown()
+
+
     
 
 
@@ -475,6 +494,6 @@ while player_points or computer_points <=3:
     #print(prediction)
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
+
+# %%
